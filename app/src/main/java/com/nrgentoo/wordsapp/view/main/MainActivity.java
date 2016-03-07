fd4cd6d37a86f47e5758c71728054d232757755d
Илья Сапсырин <nrgentoo@gmail.com>
2016-03-07 22:07:52 +0300

    first commit

---
+++ app/src/main/java/com/nrgentoo/wordsapp/view/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nrgentoo.wordsapp.view.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nrgentoo.wordsapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
